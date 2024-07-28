09d26a8119a1055a8771c8465307ec4e6fefb9ee
mariam.jamal@intercom.com.eg <mariam.jamal@intercom.com.eg>
2024-07-28 13:40:44 +0300

    Using Log4j2

--- src/main/java/com/example/librarymanagementsystem/controllers/BookController.java
+++ src/main/java/com/example/librarymanagementsystem/controllers/BookController.java
@@ -3,6 +3,7 @@ package com.example.librarymanagementsystem.controllers;
 import com.example.librarymanagementsystem.entity.Book;
 import com.example.librarymanagementsystem.service.BookInterface;
 import lombok.AllArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,33 +12,38 @@ import java.util.Optional;
 
 @RestController
 @AllArgsConstructor
-@Slf4j
+@Log4j2
 
 public class BookController {
     private BookInterface bookInterface;
     @GetMapping("/books")
     public List<Book> retrieveAll() {
-
+        log.info("retrieveAll");
         return bookInterface.retrieveAll();
     }
     @GetMapping("/books/{id}")
     public Optional<Book> findById(@PathVariable Long id) {
+        log.info("findById"+id);
         return bookInterface.findById(id);
     }
 
     @PostMapping("/books")
     public Book addBook(@RequestBody Book book) {
+        log.info("addbook"+book);
         return bookInterface.addBook(book);
     }
 
     @PutMapping("/books/{id}")
     public Book updateBook(@PathVariable Long id,@RequestBody Book book) {
+
         book.setId(id);
+        log.info("updateBook"+book);
         return bookInterface.updateBook(book);
     }
 
     @DeleteMapping("/books/{id}")
     public void deleteBook(@PathVariable Long id) {
+        log.info("deleteBook"+id);
         bookInterface.deleteBook(id);
     }
 
--- src/main/java/com/example/librarymanagementsystem/controllers/BorrowController.java
+++ src/main/java/com/example/librarymanagementsystem/controllers/BorrowController.java
@@ -3,21 +3,23 @@ package com.example.librarymanagementsystem.controllers;
 import com.example.librarymanagementsystem.entity.Borrow;
 import com.example.librarymanagementsystem.service.BorrowInterface;
 import lombok.AllArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @AllArgsConstructor
+@Log4j2
 public class BorrowController {
   private   BorrowInterface  borrowInterface;
   @PostMapping("/borrow/{bookId}/patron/{patronId}")
     public Borrow addBorrow(@RequestBody  Borrow borrow, @PathVariable Long bookId, @PathVariable Long patronId) {
-
+        log.info("borrow"+borrow);
         return borrowInterface.addBorrow(bookId,patronId,borrow);
     }
 
     @PutMapping("/return/{bookId}/patron/{patronId}")
     public Borrow updateBorrow(@RequestBody Borrow borrow, @PathVariable Long bookId, @PathVariable Long patronId) {
-
+      log.info("borrow"+borrow);
       return borrowInterface.updateBorrow(bookId,patronId,borrow);
     }
 }
